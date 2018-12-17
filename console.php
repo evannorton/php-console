53b248ee070e597aa6632a9aed75ce70a1af05e4
evan <evanan39@yahoo.com>
2018-12-17 12:51:37 -0500

    show file and line number

--- console.php
+++ console.php
@@ -1,10 +1,22 @@
 <?php
 class Console
 {
-    public function log($string)
+    public function log($input)
     {
+
+        $bt = debug_backtrace();
+        $caller = array_shift($bt);
+
+        $file = $caller["file"];
+        $file = substr($file, strrpos($file, "\\") + 1);
+        $line = $caller["line"];
+
         echo "<script type='text/javascript'>";
-        echo "console.log('%cPHP:%c$string', 'color:white;background-color:black;padding:0 .25em;', 'padding-left: .5em');";
+        echo "console.log('%c";
+        echo $file . ":" . $line;
+        echo "%c";
+        echo $input;
+        echo "', 'color:white;background-color:black;padding:0 .25em;', 'padding-left: .5em');";
         echo "</script>";
     }
 }
