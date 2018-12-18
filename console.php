b78c133151a8813eab7cf9bc6dac0f173feae2bd
evan <evanan39@yahoo.com>
2018-12-18 01:21:29 -0500

    object support, info(), warn() and error() methods

--- console.php
+++ console.php
@@ -3,13 +3,16 @@
 class Console
 {
 
-    private $array_depth = 0;
-    private $array_indent = "";
-    private $array_indent_paren = "";
+    private $depth = 0;
 
     private function wrap_script($script)
     {
         return "<script type='text/javascript'>" . $script . "</script>";
+    }
+
+    private function get_indent()
+    {
+        return str_repeat("   ", $this->depth);
     }
 
     private function format_by_type($input)
@@ -17,30 +20,35 @@ class Console
         $type = gettype($input);
         switch ($type) {
             case "array":
-                $this->array_depth++;
-                $this->array_indent .= "  ";
+                $this->depth++;
                 $contents = "Array(";
-                if ($this->array_depth === 1) {
+                if ($this->depth === 1) {
                     $contents = "\\n" . $contents;
-                } else {
-                    $this->array_indent .= "       ";
                 }
                 foreach ($input as $key => $element) {
-                    $contents .= "\\n" . $this->array_indent . "[" . $key . "] => ";
+                    $contents .= "\\n" . $this->get_indent() . "[" . $key . "] => ";
                     $contents .= $this->format_by_type($element);
                     if ($key !== count($input) - 1) {
                         $contents .= ", ";
                     }
                 }
-
-                $this->array_depth--;
-                $paren_indent_length = $this->array_depth * 9;
-
-                $paren_indent = "";
-                for ($i = 0; $i < $paren_indent_length; $i++) {
-                    $paren_indent .= " ";
+                $this->depth--;
+                $contents .= "\\n" . $this->get_indent() . ")";
+                return $contents;
+                break;
+            case "object":
+                $this->depth++;
+                $contents = get_class($input) . " Object {";
+                if ($this->depth === 1) {
+                    $contents = "\\n" . $contents;
                 }
-                $contents .= "\\n" . $paren_indent . ")";
+                $input_keys = get_object_vars($input);
+                foreach ($input_keys as $key => $value) {
+                    $contents .= "\\n" . $this->get_indent() . "[" . $key . "] => ";
+                    $contents .= $this->format_by_type($value);
+                }
+                $this->depth--;
+                $contents .= "\\n" . $this->get_indent() . "}";
                 return $contents;
                 break;
             default:
@@ -48,14 +56,12 @@ class Console
         }
     }
 
-    public function log($input)
-    {
+    function print($input, $bt, $protocol) {
         // styling
-        $console_start = "console.log('%c";
+        $console_start = "console.$protocol('%c";
         $console_end = "', 'color:white;background-color:black;padding:0 .25em;', 'padding-left: .5em');";
 
         // file name and line number
-        $bt = debug_backtrace();
         $caller = array_shift($bt);
         $file = $caller["file"];
         $file = substr($file, strrpos($file, "\\") + 1);
@@ -65,10 +71,26 @@ class Console
         // contents to log
         $contents = $this->format_by_type($input);
 
-        $this->array_depth = "";
-        $this->array_indent = "";
+        $this->depth = 0;
 
         echo $this->wrap_script($console_start . $meta . "%c" . $contents . $console_end);
+    }
+
+    public function log($input)
+    {
+        $this->print($input, debug_backtrace(), "log");
+    }
+    public function info($input)
+    {
+        $this->print($input, debug_backtrace(), "info");
+    }
+    public function warn($input)
+    {
+        $this->print($input, debug_backtrace(), "warn");
+    }
+    public function error($input)
+    {
+        $this->print($input, debug_backtrace(), "error");
     }
 
     public function clear()
