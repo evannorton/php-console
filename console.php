c43b2acdd06f9e1cf33348423b4c02ab8f99dfec
evan <evanan39@yahoo.com>
2019-03-06 10:11:54 -0500

    styling for group and groupCollapsed methods, refactoring

--- console.php
+++ console.php
@@ -3,11 +3,23 @@
 class Console
 {
     private static $depth = 0;
+    private static $call_point_styling = "', 'color:white;background-color:black;padding:0 .25em;margin-right:.5em;', '');";
     private static function wrap_script($script)
     {
         $script_element = "document.getElementById(\"php-console\")";
         $delete_script = $script_element . ".parentNode.removeChild(" . $script_element . ");";
         return "<script id=\"php-console\" type=\"text/javascript\">" . $script . $delete_script . "</script>";
+    }
+    private static function get_method()
+    {
+        return debug_backtrace()[2]["function"];
+    }
+    private static function get_call_point()
+    {
+        $bt = debug_backtrace()[2];
+        $file = substr($bt["file"], strrpos($bt["file"], DIRECTORY_SEPARATOR) + 1);
+        $line = $bt["line"];
+        return $file . ":" . $line;
     }
     private static function get_indent()
     {
@@ -48,11 +60,8 @@ class Console
                 return $contents;
                 break;
             default:
-                // unescape backslashes
                 $msg = str_replace("\\", "\\\\", $msg);
-                // escape single quotes
                 $msg = str_replace("'", "\'", $msg);
-                // remove line breaks and indents
                 $msg = preg_replace("/\r/", "\\r", $msg);
                 self::$depth++;
                 $msg = preg_replace("/\n/", "\\n" . self::get_indent(), $msg);
@@ -60,39 +69,30 @@ class Console
                 return $msg;
         }
     }
-    private static function print_log($msg, $bt, $protocol)
+    private static function print_log($msg)
     {
-        // styling
-        $console_start = "console.$protocol('%c";
-        $console_end = "', 'color:white;background-color:black;padding:0 .25em;margin-right:.5em;', '');";
-        // file name and line number
-        $caller = array_shift($bt);
-        $file = $caller["file"];
-        $file = substr($file, strrpos($file, DIRECTORY_SEPARATOR) + 1);
-        $line = $caller["line"];
-        $meta = $file . ":" . $line;
-        // contents to log
-        $contents = self::format_by_type($msg);
-        // reset indent depth
+        echo self::wrap_script("console." . self::get_method() . "('%c" . self::get_call_point() . "%c" . self::format_by_type($msg) . self::$call_point_styling);
         self::$depth = 0;
-        // echo script
-        echo self::wrap_script($console_start . $meta . "%c" . $contents . $console_end);
+    }
+    private static function print_group($label)
+    {
+        echo self::wrap_script("console." . self::get_method() . "('%c" . self::get_call_point() . "%c" . $label . self::$call_point_styling);
     }
     public static function log($msg)
     {
-        self::print_log($msg, debug_backtrace(), "log");
+        self::print_log($msg);
     }
     public static function info($msg)
     {
-        self::print_log($msg, debug_backtrace(), "info");
+        self::print_log($msg);
     }
     public static function warn($msg)
     {
-        self::print_log($msg, debug_backtrace(), "warn");
+        self::print_log($msg);
     }
     public static function error($msg)
     {
-        self::print_log($msg, debug_backtrace(), "error");
+        self::print_log($msg);
     }
     public static function clear()
     {
@@ -100,11 +100,11 @@ class Console
     }
     public static function group($label)
     {
-        echo self::wrap_script("console.group('" . $label . "');");
+        self::print_group($label);
     }
     public static function groupCollapsed($label)
     {
-        echo self::wrap_script("console.groupCollapsed('" . $label . "');");
+        self::print_group($label);
     }
     public static function groupEnd()
     {
